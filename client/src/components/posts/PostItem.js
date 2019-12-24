5c5f45c2c7f6d9cca4ff79757e0b1d17fa3e438e
Sam <samuelkbrown91@gmail.com>
2019-12-25 01:18:04 +1100

    delete post added

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -3,11 +3,12 @@ import PropTypes from 'prop-types'
 import {Link} from 'react-router-dom'
 import Moment from 'react-moment'
 import {connect} from 'react-redux'
-import {addLike, removeLike} from '../../actions/post'
+import {addLike, removeLike, deletePost} from '../../actions/post'
 
 const PostItem = ({
     addLike,
     removeLike,
+    deletePost,
     auth,
     post: {
         _id,
@@ -53,7 +54,8 @@ const PostItem = ({
               )}
             </Link>
             {!auth.loading && user === auth.user._id && (
-                <button      
+                <button
+                    onClick={e => deletePost(_id)}      
                     type="button"
                     class="btn btn-danger">
                     <i class="fas fa-times"></i>
@@ -65,11 +67,14 @@ const PostItem = ({
 
 PostItem.propTypes = {
     post: PropTypes.object.isRequired,
-    auth: PropTypes.object.isRequired
+    auth: PropTypes.object.isRequired,
+    addLike: PropTypes.func.isRequired,
+    removeLike: PropTypes.func.isRequired,
+    deletePost: PropTypes.func.isRequired
 }
 
 const mapStateToProps = state => ({
     auth: state.auth
 })
 
-export default connect(mapStateToProps, {addLike, removeLike})(PostItem)
+export default connect(mapStateToProps, {addLike, removeLike, deletePost})(PostItem)
