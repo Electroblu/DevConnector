ef9f9b526a5491b420b38f9ad62f9c5290b82cf2
Sam <samuelkbrown91@gmail.com>
2019-12-24 16:54:29 +1100

    create and edit profile

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
 import React, {Fragment, useEffect} from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+// Routes
+import PrivateRoute from './components/routing/PrivateRoute';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Alert from './components/layout/Alert';
 import Dashboard from './components/dashboard/Dashboard';
-import PrivateRoute from './components/routing/PrivateRoute';
+import CreateProfile from './components/profile-forms/CreateProfile';
+import EditProfile from './components/profile-forms/EditProfile';
 // Redux
 import {Provider} from 'react-redux';
 import store from './store';
@@ -36,6 +40,8 @@ const App = () => {
               <Route exact path="/register" component={Register}/>
               <Route exact path="/login" component={Login}/>
               <PrivateRoute exact path="/dashboard" component={Dashboard}/>
+              <PrivateRoute exact path="/create-profile" component={CreateProfile}/>
+              <PrivateRoute exact path="/edit-profile" component={EditProfile}/>
             </Switch>
           </section>
         </Fragment>
