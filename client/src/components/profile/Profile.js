07436d85e0e04fc3a5ada96f626ef63e89c0cc8b
Sam <samuelkbrown91@gmail.com>
2019-12-24 22:52:19 +1100

    profile top and about created

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -3,7 +3,9 @@ import PropTypes from 'prop-types'
 import {Link} from 'react-router-dom'
 import {connect} from 'react-redux'
 import Spinner from '../layout/Spinner'
-import {getProfileById, getCurrentProfile} from '../../actions/profile'
+import ProfileTop from './ProfileTop'
+import ProfileAbout from './ProfileAbout'
+import {getProfileById} from '../../actions/profile'
 
 const Profile = ({
     getProfileById,
@@ -13,7 +15,7 @@ const Profile = ({
 }) => {
     useEffect(() => {
         getProfileById(match.params.id);
-    }, [getProfileById, match.parmas.id])
+    }, [getProfileById, match.params.id])
 
     return (
         <Fragment>
@@ -24,6 +26,10 @@ const Profile = ({
                 auth.user._id === profile.user._id && (
                     <Link to='/edit-profile' className='btn btn-dark'>Edit Profile</Link>
                 )}
+                <div class="profile-grid my-1">
+                    <ProfileTop profile={profile}/>
+                    <ProfileAbout profile={profile}/>
+                </div>
                 </Fragment>}
         </Fragment>
     )
